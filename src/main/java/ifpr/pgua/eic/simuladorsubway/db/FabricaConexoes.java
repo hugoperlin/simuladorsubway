0d3cf03589fd837d8e4ddbf6d3b8bf9690bbd7dd
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-03-14 15:13:32 -0300

    utilizando properties

--- src/main/java/ifpr/pgua/eic/simuladorsubway/db/FabricaConexoes.java
+++ src/main/java/ifpr/pgua/eic/simuladorsubway/db/FabricaConexoes.java
@@ -1,28 +1,41 @@
 package ifpr.pgua.eic.simuladorsubway.db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexoes {
 
     private static final int MAX_CONNECTIONS = 5;
     //jdbc:mysql://infoguaifpr.com.br:3136
-    //private static final String URL = "jdbc:mysql://localhost:3306/sanduiche";
-    //private static final String URL = "jdbc:sqlite:sanduiche.sqlite";
-    private static final String URL = "jdbc:postgresql://localhost:5432/sanduiche";
+    private static String URL;
 
-    private static final String USERNAME = "user";
-    private static final String PASSWORD = "12345";
-
-
-
+    private static String USERNAME;
+    private static String PASSWORD;
 
     private Connection[] connections;
 
     private static FabricaConexoes instance = new FabricaConexoes();
 
     private FabricaConexoes(){
+
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getResourceAsStream("/config/config.properties");
+
+        try{
+            properties.load(inputStream);
+
+            URL = properties.getProperty("DB_URL");
+            USERNAME = properties.getProperty("DB_USERNAME");
+            PASSWORD = properties.getProperty("DB_PASSWORD");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
 
         connections = new Connection[MAX_CONNECTIONS];
 
